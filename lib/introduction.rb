9b8ed5458b4f7325d6cd338dfafe4428601fcaf5
Rob Nute <robnute@gmail.com>
2018-10-25 00:20:44 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,10 +2,8 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
-introduction (Josh)
-
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning #{language}."
 end
 
-introduction_with_language (Dan, Ember.JS)
+
